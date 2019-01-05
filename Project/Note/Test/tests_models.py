dc117b6a4637ac92e3613df67c10f6649ac094b7
mwuillai <wuillaimathieu78@gmail.com>
2019-01-05 18:29:15 +0100

    implementation of category models

--- Project/Note/Test/tests_models.py
+++ Project/Note/Test/tests_models.py
@@ -1,13 +1,51 @@
 from django.test import TestCase
 from mixer.backend.django import mixer
 import Note.models as models
+from datetime import datetime, timedelta
+import pytz
+from Project.settings import TIME_ZONE
 
 
 class TestNotes(TestCase):
     """
     Tests of my notes class
     """
+    def setUp(self):
+        mixer.blend('Note.notes')
 
     def test_instance_creation(self):
-        note = mixer.blend('Note.notes')
+        """
+        Test if there is an error after instance creation
+        """
+        note = models.Notes.objects.get(pk=1)
         self.assertIsInstance(note, models.Notes)
+        self.assertTrue(note)
+
+    def test_creation_date(self):
+        """
+        Test if creation date is Ok
+        """
+        now = datetime.now(pytz.timezone(TIME_ZONE))
+        note = models.Notes.objects.get(pk=1)
+        created_date = note.created_at
+        created_date.replace(tzinfo=None)
+        self.assertTrue((now - timedelta(seconds=50)) <= created_date <= now)
+
+
+class TestCategory(TestCase):
+    """
+    Tests of my Category class
+    """
+    def setUp(self):
+        mixer.blend('Note.category')
+    
+    def test_instance_creation(self):
+        categorie = models.Category.objects.get(pk=1)
+        self.assertIsInstance(categorie, models.Category)
+
+    def test_creation_date(self):
+        now = datetime.now(pytz.timezone(TIME_ZONE))
+        categorie = models.Category.objects.get(pk=1)
+        created_date = categorie.created_at
+        created_date.replace(tzinfo=None)
+        self.assertTrue((now - timedelta(seconds=50)) <= created_date <= now)
--- Project/Note/models.py
+++ Project/Note/models.py
@@ -1,5 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
 class Notes(models.Model):
-    name = models.CharField(max_length=30)
+    """
+    Class of the notes model. they can have a body and a tittle.
+    Many categories can be attach to a notes, and only one user can be attach to it.
+    """
+    title = models.CharField(max_length=30)
+    body = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+
+class Category(models.Model):
+    """
+    Class of the category model. they can have a tittle.
+    Many notes can be attach to a category, and only one user can be attach to it.
+    """
+    title = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    notes_id = models.ManyToManyField(Notes)
