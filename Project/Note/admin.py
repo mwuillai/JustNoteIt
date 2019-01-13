c11b7147ed41f93fb2ab73be3b6da2c006f9c06c
mwuillai <wuillaimathieu78@gmail.com>
2019-01-13 12:00:18 +0100

    add Notes and category to admin and str method and Meta class to models

--- Project/Note/admin.py
+++ Project/Note/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from Note.models import Category, Notes
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Notes)
--- Project/Note/models.py
+++ Project/Note/models.py
@@ -8,11 +8,21 @@ class Notes(models.Model):
     Class of the notes model. they can have a body and a tittle.
     Many categories can be attach to a notes, and only one user can be attach to it.
     """
+
+
     title = models.CharField(max_length=30)
     body = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+    class Meta:
+        verbose_name = ("Note")
+        verbose_name_plural = ("Notes")
+        ordering = ("-updated_at", )
+
+    def __str__(self):
+        return self.title
 
 
 class Category(models.Model):
@@ -20,8 +30,17 @@ class Category(models.Model):
     Class of the category model. they can have a tittle.
     Many notes can be attach to a category, and only one user can be attach to it.
     """
+
     title = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     notes_id = models.ManyToManyField(Notes)
+
+    class Meta:
+        verbose_name = ("Category")
+        verbose_name_plural = ("Categories")
+        ordering = ("-updated_at", )
+
+    def __str__(self):
+        return self.title
